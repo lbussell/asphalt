fb9863084ee38e6b5159f0856a191bb701c8282d
Logan Bussell <github@loganbussell.com>
2026-04-30 14:19:59 -0700

    Fix formatting

--- src/Classlib/Class1.cs
+++ src/Classlib/Class1.cs
@@ -1,4 +1,7 @@
-﻿namespace Classlib;
+﻿// SPDX-FileCopyrightText: Copyright (c) 2026 Logan Bussell
+// SPDX-License-Identifier: MIT
+
+namespace Classlib;
 
 /// <summary>
 /// Hello, I am documented!
