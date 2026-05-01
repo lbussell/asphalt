13288ce871dfbec566ae8eed5b9b929e026f8dee
Logan Bussell <github@loganbussell.com>
2026-04-30 23:31:01 -0400

    Add width, height to screen

--- src/ImtuiLib/Terminal.cs
+++ src/ImtuiLib/Terminal.cs
@@ -11,6 +11,10 @@ public readonly record struct CellPosition(int X, int Y);
 
 public readonly record struct CellStyle(AnsiColor Foreground, AnsiColor Background);
 
-public readonly record struct Screen(Cell[] Cells);
+public readonly record struct Screen(int Width, int Height, Cell[] Cells)
+{
+    public Screen(int width, int height)
+        : this(width, height, new Cell[width * height]) { }
+}
 
 public readonly record struct Size(int Width, int Height);
