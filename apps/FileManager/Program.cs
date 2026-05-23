fb4f00833a8f1c9e6c5994be551698ca936c2f33
Logan Bussell <github@loganbussell.com>
2026-05-23 15:14:32 -0700

    Add file manager app

---
+++ apps/FileManager/Program.cs
@@ -0,0 +1,49 @@
+﻿// SPDX-FileCopyrightText: Copyright (c) 2026 Logan Bussell
+// SPDX-License-Identifier: MIT
+
+using Asphalt;
+using Asphalt.Rendering;
+using Asphalt.Widgets;
+
+string currentDirectory = Directory.GetCurrentDirectory();
+
+List<FileEntry> files =
+[
+    ..Directory.GetDirectories(currentDirectory).Select(FileEntry.Directory),
+    ..Directory.GetFiles(currentDirectory).Select(FileEntry.File),
+];
+
+AsphaltApplication.Run(
+    context =>
+    {
+        using (context.HStack(grow: true))
+        {
+            using (context.Panel("Files", style: LayoutStyle.Grow))
+            {
+                foreach (FileEntry file in files)
+                {
+                    if (file.IsDirectory)
+                    {
+                        context.Text($"{file.Path}/", foregroundColor: TerminalColor.Cyan);
+                    }
+                    else
+                    {
+                        context.Text($"{file.Path}");
+                    }
+                }
+            }
+
+            using (context.Panel("Details", style: LayoutStyle.Grow))
+            {
+                context.Text("Hello details");
+            }
+        }
+    },
+    altScreen: true
+);
+
+readonly record struct FileEntry(string Path, bool IsDirectory)
+{
+    public static FileEntry Directory(string path) => new FileEntry(path, true);
+    public static FileEntry File(string path) => new FileEntry(path, false);
+}
